2a30471f3bb2a906aaf42efbbf4068502a4a008a
Jairo Naranjo <jairo_naranjo80111@hotmail.com>
2022-12-01 03:13:12 +0100

    ajuste navbar

--- frontend/src/components/views/NavbarVisit/NavbarVisit.js
+++ frontend/src/components/views/NavbarVisit/NavbarVisit.js
@@ -15,40 +15,40 @@ return(
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <Link className="Links" to="/">
           <a className="nav-link active" aria-current="page">Inicio</a>
           </Link>
-        </li>
+        </li> */}
         <li className="nav-item">
         <Link className="Links" to="/tatuadores">
-          <a className="nav-link">Tatuadores</a>
+          <a className="nav-link nav-link_tatuador">Buscar tatuadores</a>
         </Link>
-        </li>
-        <li className="nav-item dropdown">
-        <Link className="Links" to="/marketplace">
-          <a className="nav-link">Marketplace</a>
-          </Link>
         </li>
         <li className="nav-item">
           <Link className="Links" to="/">
-          <a className="nav-link active" aria-current="page">Nosotros</a>
+          <a className="nav-link active nav-link_preguntas" aria-current="page">Preguntas frecuentes</a>
           </Link>
         </li>
-        <li className="nav-item">
-          <Link className="Link" to="/login">
-          <a className="nav-link active">Iniciar sesión</a>
+        <li className="nav-item dropdown">
+        <Link className="Links" to="/">
+          <a className="nav-link">Contáctenos</a>
           </Link>
         </li>
         <li className="nav-item">
           <Link className="Link" to="/register">
-          <a className="nav-link active">Registrarse</a>
+          <a className="nav-link active">Unirme</a>
           </Link>
         </li>
+        {/* <li className="nav-item">
+          <Link className="Link" to="/">
+          <a className="nav-link active">Buscar</a>
+          </Link>
+        </li> */}
       </ul>
       <form className="d-flex" role="search">
-        {/* <input className="form-control me-2" type="search" placeholder="Buscar mi tatuador" aria-label="Search"/>
-        <button className="btn btn-search btn-outline-success" type="submit">Buscar</button> */}
+        <input className="form-control me-2" type="search" placeholder="Buscar mi tatuador" aria-label="Search"/>
+        <button className="btn btn-search btn-outline-success" type="submit">Buscar</button>
       </form>
     </div>
   </div>
